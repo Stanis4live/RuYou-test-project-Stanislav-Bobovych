3594a5c949430186e037e04667e89f1824fdbbc3
stas <chousetheone@gmail.com>
2024-04-17 13:40:06 +0300

    fix

--- color_palettes/colors/services/color_API_integration.py
+++ color_palettes/colors/services/color_API_integration.py
@@ -3,5 +3,5 @@ import requests
 def get_color_name(hex_code):
     response = requests.get(f"https://www.thecolorapi.com/id?hex={hex_code.lstrip('#')}")
     if response.status_code == 200:
-        return response.json().get('name', {}.get('value', 'Unknown'))
-    return 'Unknown'
+        return response.json().get('name', {'value': 'Unknown'})
+    return {'value': 'Unknown'}
